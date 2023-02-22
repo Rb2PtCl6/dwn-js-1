5ae59586aeb9a754e88cdf8e35d60365d2838c7f
Rb2PtCl6 <ggmetalgg2@gmail.com>
2023-02-22 12:03:03 +0200

    links' sourc is txt file

--- extract.js
+++ extract.js
@@ -2,8 +2,6 @@ const { spawn }=require("child_process");
 const fs = require('fs');
 const path=require('path');
 
-
-
 //short https://youtu.be/hH6yu19Ui4s
 //full https://www.youtube.com/watch?v=X2DUpDxFJyg
 //music https://music.youtube.com/watch?v=I7RFELL_2dA
@@ -35,7 +33,7 @@ function getFileName(video_id,location){
         }
     }
 }
-function downloader(type, arg, video_link){
+function downloader(type, arg, video_link,parsing_result){
     /*if (arg!="bestvideo" || arg=="bestaudio"){
         console.log(`${arg} is incorrect argument. Correct argument is bestaudio or bestvideo`)
         return
@@ -69,12 +67,13 @@ function downloader(type, arg, video_link){
     });
 }
 function audio_converter(video_src,folder){
-    var mp3link;
+    /*var mp3link;
     if (path.extname(video_src)==".webm"){
         mp3link=video_src.replace(".webm",".mp3")
     } else if (path.extname(video_src)==".mp4"){
         mp3link=video_src.replace(".mp4",".mp3")
-    }
+    }*/
+    var mp3link=path.basename(video_src,path.extname(video_src))+".mp3"
     const action=spawn("ffmpeg", ["-i", folder+"/"+video_src, folder+"/"+mp3link])
     action.stdout.on("data", data => {
         console.log(`stdout: ${data}`);
@@ -93,14 +92,14 @@ function audio_converter(video_src,folder){
     });
 }
 function extractor(video_src,folder){
-    var jpglink;
-
+    /*var jpglink;
     if (path.extname(video_src)==".webm"){
         jpglink=video_src.replace(".webm",".jpg")
     } else if (path.extname(video_src)==".mp4"){
         jpglink=video_src.replace(".mp4",".jpg")
-    }
-    console.log(jpglink+" "+ typeof jpglink)
+    }*/
+    var jpglink=path.basename(video_src,path.extname(video_src))+".jpg";
+    //console.log(jpglink+" "+ typeof jpglink)
 
     const action=spawn("ffmpeg", ["-i", folder+"/"+video_src, "-frames:v", "1",folder+"/"+jpglink])
     action.stdout.on("data", data => {
@@ -119,19 +118,25 @@ function extractor(video_src,folder){
         fs.unlinkSync(folder+"/"+jpglink)
     });
 }
-
-// main
-for(var zone=1;zone<2; zone++){
-var local_link="https://www.youtube.com/watch?v=I7RFELL_2dA";
-if (local_link=="1"){
-    break
+function links_from_source(){
+    var out=(fs.readFileSync('source.txt','utf-8',function(err,data){
+        if (err) return;
+        return data
+    })).split('\r\n')
+    return out
+//var out2=out1
 }
-var parsing_result=link_parser(local_link)
+function proc(link){ //code which process video
+var parsing_result=link_parser(link)
 if (parsing_result=="error"){
     console.log("Your input is not link or your link is incorrect!")
-    continue
+    return
 }
-downloader("audio","bestaudio",local_link)
-downloader("video","bestvideo",local_link)
+downloader("audio","bestaudio",link,parsing_result)
+downloader("video","bestvideo",link,parsing_result)
 }
+
+// main
+var link_arr=links_from_source()
+for (var j in link_arr){proc(link_arr[j])}
 //console.log("The end!")
